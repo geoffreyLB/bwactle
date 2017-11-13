a51f1c38e10a985ed515b2d74d099a32e2ebe8e7
Geoffrey <geoffrey.leborges@epitech.eu>
2017-11-13 16:41:14 +0100

    get current player

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import IO from 'socket.io-client';
-import { LoginPage } from '../login/login';
+import {LoginPage} from '../login/login';
+import {platformBrowser} from '@angular/platform-browser/src/browser';
 
 /**
  * Generated class for the GamePage page.
@@ -11,40 +12,48 @@ import { LoginPage } from '../login/login';
  */
 
 @IonicPage()
-@Component({
-  selector: 'page-game',
-  templateUrl: 'game.html',
-})
+@Component({selector: 'page-game', templateUrl: 'game.html'})
 
 export class GamePage extends LoginPage {
-public row = [];
-public col = [];
-public limitRow = 20;
-public limitCol = 20;
-public player;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public row = [];
+  public col = [];
+  public limitRow = 20;
+  public limitCol = 20;
+  public player;
+  public currentPlayer;
+
+  constructor(public navCtrl : NavController, public navParams : NavParams) {
     super(navCtrl);
+    const that = this;
 
     const username = navParams.get('username');
     const password = navParams.get('password');
     const apiUrl = 'wac.epitech.eu:1337';
 
-    for (var i = 1; i <= this.limitRow; i++) {
-      this.row.push(i);
+    for (var i = 0; i <= this.limitRow; i++) {
+      this
+        .row
+        .push(i);
     }
 
-    for (var j = 1; j <= this.limitCol; j++) {
-      this.col.push(i);
+    for (var j = 0; j <= this.limitCol; j++) {
+      this
+        .col
+        .push(j);
     }
-  
-   let socket = IO(apiUrl, {query: `login=${username}&pwd=${password}`});
-   
-   socket.on('player/add', function(player) {
-    if (player.login === username) {
-       this.currentPlayer = player;
-      console.log(this.currentPlayer)
-    }
-    }); 
+
+    let socket = IO(apiUrl, {query: `login=${username}&pwd=${password}`});
+
+    socket.on('player/add', function (player) {
+      if (player.login === username) {
+          that.getPlayer(player);
+      }
+    });
   }
+
+  getPlayer(player: string) {
+    this.currentPlayer = player;
+  }
+
 }
