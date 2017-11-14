11b8fb96f8848d32d99b287b4515752471641b10
Geoffrey <geoffrey.leborges@epitech.eu>
2017-11-14 16:48:20 +0100

    rebase

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -1,14 +1,8 @@
-import {Component, HostListener} from '@angular/core';
-import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import { Component, HostListener } from '@angular/core';
+import {IonicPage, NavController, NavParams } from 'ionic-angular';
 import IO from 'socket.io-client';
-import {LoginPage} from '../login/login';
 
-/**
- * Generated class for the GamePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { LoginPage } from '../login/login';
 
 @IonicPage()
 @Component({selector: 'page-game', templateUrl: 'game.html'})
@@ -20,18 +14,39 @@ export class GamePage extends LoginPage {
   public limitRow = 40;
   public limitCol = 40;
   public players = {};
-  public currentPlayer;
   public playerPositionX;
   public playerPositionY;
+  public playerLogin;
   public playerLife;
-  // public arrayPlayers = [];
   public socket;
   public Object = Object;
 
   @HostListener('document:keypress', ['$event'])
-  handleKeyboardEvent(event : KeyboardEvent) {
-    console.log(event)
-  }
+  handleKeyboardEvent(event: KeyboardEvent) {
+    let direction;
+
+    switch (event.which) {
+      case 113: 
+        direction = "left";
+        break;
+      case 122: 
+        direction = "up";
+        break;
+      case 100: 
+        direction = "right";
+        break;
+      case 115: 
+        direction = "down";
+        break;
+      default:
+        return;
+    }
+    this.newDirection(direction);
+}
+
+newMove(direction) {
+ 
+}
 
   constructor(public navCtrl : NavController, public navParams : NavParams) {
     super(navCtrl);
@@ -41,34 +56,40 @@ export class GamePage extends LoginPage {
     const password = navParams.get('password');
     const apiUrl = 'wac.epitech.eu:1337';
     
-    for (var i = 0; i <= this.limitRow; i++) { this.row.push(i);}
-    for (var j = 0; j <= this.limitCol; j++) { this.col.push(j);}
+    for (let i = 0; i <= this.limitRow; i++) {console.log('toto'); this.row.push(i);}
+    for (let j = 0; j <= this.limitCol; j++) { this.col.push(j);}
     
     this.socket = IO(apiUrl, {query: `login=${username}&pwd=${password}`});
     
     this.socket.on('player/move', function (player) {
-        // that.arrayPlayers.map(function (value) {
-        //     if (value.login === players.login) {
-        //         value['x'] = players.x,
-        //         value['y'] = players.y
-        //     }
-        //   });
-        console.log('move')
         that.players[player.login].x = player.x;
         that.players[player.login].y = player.y;
       });
 
     this.socket.on('player/add', function (player) {
-        // that.arrayPlayers.push(player)
         that.players[player.login] = player;
         if (player.login === username) {
-          that.getPlayer(player);
+          that.currentPlayer(player);
         }
       });
   }
 
-  getPlayer(player : { login: string, x: number, y: number}) {
+  currentPlayer(player : { login: string, x: number, y: number}) {
     this.playerPositionX = player.x;
     this.playerPositionY = player.y;
+    this.playerLogin = player.login;
   }
+
+  newDirection(direction: string) {
+    const that = this;
+
+    this.socket.emit('move', direction);
+
+    this.socket.on('player/move', function(player) {
+     if (player.login === 'MrPink') {
+      that.currentPlayer(player)
+     }
+    });
+  }
+
 }
