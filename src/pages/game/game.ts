8d768f3f213dbeb5366f301ab0c6af22e6a1612e
Geoffrey <geoffrey.leborges@epitech.eu>
2017-11-14 15:57:42 +0100

    array delete and object instead

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -19,70 +19,56 @@ export class GamePage extends LoginPage {
   public col = [];
   public limitRow = 40;
   public limitCol = 40;
-  public players;
+  public players = {};
   public currentPlayer;
   public playerPositionX;
   public playerPositionY;
   public playerLife;
-  public arrayPlayers = [];
+  // public arrayPlayers = [];
   public socket;
+  public Object = Object;
 
   @HostListener('document:keypress', ['$event'])
-  handleKeyboardEvent(event: KeyboardEvent) { 
+  handleKeyboardEvent(event : KeyboardEvent) {
     console.log(event)
   }
 
   constructor(public navCtrl : NavController, public navParams : NavParams) {
     super(navCtrl);
+    
     const that = this;
-
     const username = navParams.get('username');
     const password = navParams.get('password');
     const apiUrl = 'wac.epitech.eu:1337';
-
-    for (var i = 0; i <= this.limitRow; i++) {
-      this
-        .row
-        .push(i);
-    }
-
-    for (var j = 0; j <= this.limitCol; j++) {
-      this
-        .col
-        .push(j);
-    }
-
+    
+    for (var i = 0; i <= this.limitRow; i++) { this.row.push(i);}
+    for (var j = 0; j <= this.limitCol; j++) { this.col.push(j);}
+    
     this.socket = IO(apiUrl, {query: `login=${username}&pwd=${password}`});
+    
+    this.socket.on('player/move', function (player) {
+        // that.arrayPlayers.map(function (value) {
+        //     if (value.login === players.login) {
+        //         value['x'] = players.x,
+        //         value['y'] = players.y
+        //     }
+        //   });
+        console.log('move')
+        that.players[player.login].x = player.x;
+        that.players[player.login].y = player.y;
+      });
 
     this.socket.on('player/add', function (player) {
-      that.displayPlayers(player);
-      if (player.login === username) {
-        that.getPlayer(player);
-      }
-    });
+        // that.arrayPlayers.push(player)
+        that.players[player.login] = player;
+        if (player.login === username) {
+          that.getPlayer(player);
+        }
+      });
   }
 
-  getPlayer(player : {
-    login: string,
-    x: number,
-    y: number
-  }) {
-   this.playerPositionX = player.x;
-   this.playerPositionY = player.y;
-   
+  getPlayer(player : { login: string, x: number, y: number}) {
+    this.playerPositionX = player.x;
+    this.playerPositionY = player.y;
   }
-
-  displayPlayers(player) {
-    this.arrayPlayers.push(player)
-    this.players = player;
-    const that = this;
-    console.log( this.arrayPlayers)
-
-    this.socket.on('player/move', function(players) {
-      console.log(players)
-        that.arrayPlayers.push(players)
-    });
-
-  }
-
 }
